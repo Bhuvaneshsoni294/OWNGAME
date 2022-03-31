8f9bef69aa127e9ff4a0ae634da759aeea173de2
Bhuvaneshsoni294 <89522795+Bhuvaneshsoni294@users.noreply.github.com>
2022-03-31 06:49:28 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var spaceship,astroid,alian;
+ var bgImg
+
+
+function setup() {
+  createCanvas(displayWidth,displayHight);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
